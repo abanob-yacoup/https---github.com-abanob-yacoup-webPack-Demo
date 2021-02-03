eeb3c53a791c4ba2478583537bf29b43c7af1cf6
Abanob Yacoub <abanob.y.adly@gmail.com>
2021-02-04 01:12:55 +0200

    uploading demo files

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import hello from './hello wolrd.js';
+hello()
+import './component/header/header'
+import './style.css';
+
+
+import pic from'./assets/photo1.jpg'
+const img = document.createElement("img");
+img.src=pic;
+document.body.appendChild(img);
+
